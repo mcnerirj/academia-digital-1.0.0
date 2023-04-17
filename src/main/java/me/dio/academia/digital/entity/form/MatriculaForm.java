8321def8e7922a4d5d55944f4d20dad679da9f5c
mcnerirj <mcneri@gmail.com>
2023-04-16 22:31:58 -0300

    Subindo a primeira versao

---
+++ src/main/java/me/dio/academia/digital/entity/form/MatriculaForm.java
@@ -0,0 +1,14 @@
+package me.dio.academia.digital.entity.form;
+
+public class MatriculaForm {
+
+  private Long alunoId;
+
+  public Long getAlunoId() {
+    return alunoId;
+  }
+
+  public void setAlunoId(Long alunoId) {
+    this.alunoId = alunoId;
+  }
+}
